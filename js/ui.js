adddb4a19f98a77481403e06cc1952c1fd2368c8
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-11 19:51:56 +0300

    Make a function (displayWeather) UI Layer

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,13 @@
 // For the Data (UI Layer)
 
-    
+
+function displayWeather(data){
+    const result = document.getElementById("weatherResult")
+
+    result.innerHTML = `
+        <h2 class="text-xl font-bold">${data.name}</h2>
+        <p class="text-gray-400">${data.weather[0].description}</p>
+        <p class="text-3xl mt-2">${data.main.temp}</p>
+    `
+}
+
