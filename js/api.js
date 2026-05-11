9f61b227cb4dce5b12c506e224199ef542b6db31
Marwan-Mostafa <marwan.mf.tech@gmail.com>
2026-05-11 19:41:55 +0300

    Fetch API of the Weather

--- js/api.js
+++ js/api.js
@@ -1,1 +1,16 @@
 // For API call
+
+const API_KEY = "The key"
+
+async function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+    const response = await fetch(url)
+
+    if(!response.ok){
+        throw new Error("City Not Found")
+    }
+
+    const data = await response.json()
+    return data
+}
